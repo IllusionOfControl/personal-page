991c7f83506474ae78d6777a5fb316849f7a1636
Illusion of control <sergeyskorokhod2@gmail.com>
2023-02-11 13:57:57 +0300

    redesigned layout block of projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import Link from "next/link";
 import {FaGithub, FaLink} from 'react-icons/fa'
 import {projects} from '../../data'
@@ -10,37 +9,28 @@ const Hero = () => {
       <div
         className="max-w-[1240px] grid grid-cols-1 sm:grid-cols-[2fr_4fr] px-4 mx-auto md:px-10 py-16 sm:py-28 gap-4">
         <h1 className="text-5xl w-full">Featured <span className="text-rose-800">projects</span></h1>
-        <ul className="sm:pt-16">
+        <ul className="sm:pt-24 grid grid-cols-2 gap-8">
           {projects.map((project, i) => {
             return (
-              <li key={i} className="py-4">
+              <li key={i} className="rounded-md border shadow-md border-neutral-700 hover:border-rose-900 transition-colors">
                 <div
-                  className="h-full rounded-xl flex flex-col justify-between border shadow-md border-neutral-700">
-                  <div className="flex flex-col">
-                    <div
-                      className="relative h-[200px] w-full rounded-t-xl bg-neutral-800 lg:h-[300px]">
-                      <Image src={project.img} alt="Portfolio" width={700} height={700}
-                             className="absolute h-full w-full z-10 rounded-t-xl object-cover object-top"/>
-                    </div>
-                    <div className="h-fit p-5 space-y-3">
-                      <div className="flex items-center justify-between space-x-3">
+                  className="h-full flex flex-col justify-between">
+                  <div className="flex flex-col flex-grow h-fit p-5 space-y-3">
+                      <div className="flex items-center justify-between">
                         <p className="text-lg font-bold text-primary">{project.title}</p>
                         <div className="flex space-x-4 text-3xl">
-                          {project.links.github && (<Link href={project.links.github} className="hover:text-rose-800 transition-colors"><FaGithub/></Link>)}
-                          {project.links.demo && (<Link href={project.links.demo} className="hover:text-rose-800 transition-colors"><FaLink/></Link>)}
+                          {project.links.github && (<Link href={project.links.github} className="hover:text-rose-700 transition-colors"><FaGithub/></Link>)}
+                          {project.links.demo && (<Link href={project.links.demo} className="hover:text-rose-700 transition-colors"><FaLink/></Link>)}
                         </div>
                       </div>
-                      <p className="text-sm md:text-base">{project.description}</p>
-                      <div className="w-full flex flex-col items-center space-y-3">
-                        <div className="w-full flex flex-wrap">
+                      <p className="text-sm md:text-base grow">{project.description}</p>
+                        <div className="w-full  flex flex-wrap align-bottom">
                           {project.stack.map((technology, i) =>
-                            <div key={i} className="px-3 mx-1 my-2 rounded-xl bg-neutral-700">{technology}</div>)
+                            <div key={i} className="px-3 mx-1 my-1 rounded-xl bg-neutral-700">{technology}</div>)
                           }
                         </div>
-                      </div>
                     </div>
                   </div>
-                </div>
               </li>
             )
           })}
