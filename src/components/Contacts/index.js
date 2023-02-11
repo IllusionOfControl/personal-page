84cbf0b2a2ee11b5ad16d11720e983355c49ce61
Illusion of control <sergeyskorokhod2@gmail.com>
2023-02-11 13:58:41 +0300

    added mouse hover effects and changed links

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -3,13 +3,11 @@ import {
   FaTelegram,
   FaGithub,
   FaInstagram,
-  FaTwitter,
   FaLinkedin,
-  FaDiscord,
   FaEnvelope
 } from 'react-icons/fa'
 import Link from "next/link";
-import {socials} from "../../data";
+import {socials, location} from "../../data";
 
 const Contacts = () => {
   return (
@@ -18,14 +16,12 @@ const Contacts = () => {
         <div className="max-w-[1240px] grid grid-cols-1 sm:grid-cols-[2fr_4fr] px-4 mx-auto md:px-10">
           <h1 className="text-5xl pb-4">Contact with <span className="text-rose-800">me</span></h1>
           <div className="max-w-[700px] mx-auto pt-16 sm:pt-20 grid grid-cols-1 sm:grid-cols-2 text-xl gap-4">
-            <p><span><FaSearchLocation className="inline mr-2"/></span>Belarus, Brest</p>
-            <Link href={socials.email_link}><FaEnvelope className="inline mr-2"/>{socials.email}</Link>
-            <Link href={socials.telegram_link}><FaTelegram className="inline mr-2"/>{socials.telegram}</Link>
-            <Link href={socials.github_link}><FaGithub className="inline mr-2"/>{socials.github}</Link>
-            <Link href={socials.instagram_link}><FaInstagram className="inline mr-2"/>{socials.instagram}</Link>
-            <p><span><FaTwitter className="inline mr-2"/></span>Twitter</p>
-            <p><span><FaLinkedin className="inline mr-2"/></span>LinkedIn</p>
-            <p><span><FaDiscord className="inline mr-2"/></span>Discord</p>
+            <Link className="hover:text-rose-600 transition-colors" href={location.link}><FaSearchLocation className="inline mr-2"/>{location.title}</Link>
+            <Link className="hover:text-rose-600 transition-colors" href={socials.email_link}><FaEnvelope className="inline mr-2"/>{socials.email}</Link>
+            <Link className="hover:text-rose-600 transition-colors" href={socials.telegram_link}><FaTelegram className="inline mr-2"/>{socials.telegram}</Link>
+            <Link className="hover:text-rose-600 transition-colors" href={socials.github_link}><FaGithub className="inline mr-2"/>{socials.github}</Link>
+            <Link className="hover:text-rose-600 transition-colors" href={socials.instagram_link}><FaInstagram className="inline mr-2"/>{socials.instagram}</Link>
+            <Link className="hover:text-rose-600 transition-colors" href={socials.linkedin_link}><FaLinkedin className="inline mr-2"/>{socials.linkedin}</Link>
           </div>
         </div>
       </div>
--- src/data/index.js
+++ src/data/index.js
@@ -10,7 +10,6 @@ const skills = [
 const projects = [
   {
     title: "Todoist clone",
-    img: "/projects/todoist-clone.jpg",
     stack: ["React", "Tailwind", "PostgreSQL", "FastAPI"],
     description: "Todoist is a web service and a set of task management software. This project implements a copy of it.",
     links: {
@@ -19,16 +18,42 @@ const projects = [
     }
   },
   {
-    title: "Lorem ipsum",
-    img: "/projects/example.jpg",
-    stack: ["React", "JavaScript", "MongoDB", "Tailwind"],
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    title: "USB Computer Status Display",
+    stack: ["С", "Microcontrollers/PIC", "USB", "C#"],
+    description:  "The device displays system information about the status of the computer: " +
+                  "CPU, Ram, Disk, Network load, uptime, world datetime. " +
+                  "The device consists of a PIC18 microcontroller and a 40x04 display and a USB adapter.",
     links: {
       github: "https://github.com/",
       demo: "https://example.com/"
     }
   },
+  {
+    title: "Personal page",
+    stack: ["Next.js", "JavaScript", "Tailwind"],
+    description: "A static website to present me & my projects. My first use of the Next.js framework",
+    links: {
+      github: "https://github.com/",
+      demo: "https://example.com/"
+    }
+  },
+  {
+    title: "Todoist clone",
+    img: "/projects/todoist-clone.jpg",
+    stack: ["React", "Tailwind", "PostgreSQL", "FastAPI"],
+    description: "Todoist is a web service and a set of task management software. This project implements a copy of it.",
+    links: {
+      github: "https://github.com/IllusionOfControl/todoist-clone",
+      // demo: "https://example.com/"
+    }
+  },
+
 ]
+
+const location = {
+  title: "Belarus, Brest",
+  link: "https://goo.gl/maps/BAi5Zq9nxcWkNVpi9",
+};
 
 const socials = {
   email: "sergeyskorokhod2@gmail.com",
@@ -38,7 +63,9 @@ const socials = {
   instagram: "@illus1on_of_control",
   instagram_link: "https://instagram.com/illusqon_of_control",
   github: "IllusionOfControl",
-  github_link: "https://github.com/IllusionOfControl"
+  github_link: "https://github.com/IllusionOfControl",
+  linkedin: "Sergey Skorokhod",
+  linkedin_link: "https://www.linkedin.com/in/skorokhod-sergey/",
 }
 
-export {skills, projects, socials};
+export {skills, projects, socials, location};
