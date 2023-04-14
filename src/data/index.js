7d0d4ce7c17066631af7af02625bd4d3dda53e95
Sergey Skorokhod <sergeyskorokhod2@gmail.com>
2023-04-14 15:16:09 +0300

    added languages to projects.

--- src/data/index.js
+++ src/data/index.js
@@ -12,7 +12,7 @@ const cvPdfUrl = "https://drive.google.com/file/d/17oqcjAg94V_Kgj7geIr3CN2pF_QDL
 const projects = [
   {
     title: "Personal page",
-    stack: ["Next.js", "JavaScript", "Tailwind"],
+    stack: ["JavaScript", "Next.js", "JavaScript", "Tailwind"],
     description: "A static website to present me & my projects. My first use of the Next.js framework",
     links: {
       github: "https://github.com/IllusionOfControl/personal-portfolio",
@@ -21,7 +21,7 @@ const projects = [
   },
   {
     title: "React projects",
-    stack: ["React", "Tailwind", "Redux"],
+    stack: ["JavaScript", "React", "Tailwind", "Redux"],
     description: "A wide variety of small projects using React.",
     links: {
       github: "https://github.com/IllusionOfControl/react-projects",
@@ -29,7 +29,7 @@ const projects = [
   },
   {
     title: "Todoist clone",
-    stack: ["React", "Tailwind", "PostgreSQL", "FastAPI"],
+    stack: ["JavaScript", "Python", "React", "Tailwind", "PostgreSQL", "FastAPI"],
     description: "Todoist is a web service and a set of task management software. This project implements a copy of it.",
     links: {
       github: "https://github.com/IllusionOfControl/todoist-clone",
@@ -38,10 +38,10 @@ const projects = [
   {
     title: "USB Computer Status Display",
     stack: ["C#", "С", "Microcontrollers/PIC", "USB"],
-    description:  "The device displays system information about the status of the computer: " +
-                  "CPU, Ram, Disk, Network load, uptime, world datetime. " +
-                  "The device consists of a PIC18 microcontroller and a 40x04 display and a USB adapter. " +
-                  "For the Windows system only!",
+    description: "The device displays system information about the status of the computer: " +
+      "CPU, Ram, Disk, Network load, uptime, world datetime. " +
+      "The device consists of a PIC18 microcontroller and a 40x04 display and a USB adapter. " +
+      "For the Windows system only!",
     links: {
       github: "https://github.com/IllusionOfControl/UCSD-PIC-Source",
     }
@@ -49,8 +49,8 @@ const projects = [
   {
     title: "BSUIR repo",
     stack: ["Algorithms", "Data Structures", "Computer Graphics", "Databases"],
-    description:  "The repository contains laboratory and practical work done at the university. " +
-                  "Contains a variety of computer science examples",
+    description: "The repository contains laboratory and practical work done at the university. " +
+      "Contains a variety of computer science examples",
     links: {
       github: "https://github.com/IllusionOfControl/todoist-clone",
     }
