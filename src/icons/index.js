62fead24aa4aa6d697152268691ad4485e629772
Sergey Skorokhod <sergeyskorokhod2@gmail.com>
2025-09-29 13:42:47 +0300

    add icons

--- src/icons/index.js
+++ src/icons/index.js
@@ -4,22 +4,37 @@ import ReactIcon from "./react";
 import FastApiIcon from "./fastapi";
 import FlaskIcon from "./flask";
 import DjangoIcon from "./django";
+import ReduxIcon from "./redux";
+import PostgreSqlIcon from "./postgresql";
+import RedisIcon from "./redis";
+import MongoDbIcon from "./mongodb";
+import DockerIcon from "./docker";
+import GitIcon from "./git";
+import PostmanIcon from "./postman";
+import BashIcon from "./bash";
+import PyTestIcon from "./pytest";
 
 
 const mapIconWithComponent = {
   react: ReactIcon,
+  redux: ReduxIcon,
   nextjs: NextJsIcon,
   tailwindcss: TailwindCssIcon,
   fastapi: FastApiIcon,
   flask: FlaskIcon,
   django: DjangoIcon,
+  postgresql: PostgreSqlIcon,
+  redis: RedisIcon,
+  mongodb: MongoDbIcon,
+  docker: DockerIcon,
+  git: GitIcon,
+  postman: PostmanIcon,
+  bash: BashIcon,
+  pytest: PyTestIcon,
 }
 
 const PortfolioIcon = ({icon}) => {
-  console.log(icon);
   const iconComponent = mapIconWithComponent[icon]
-
-  console.log(iconComponent);
 
   if (!iconComponent) {
     console.warn(`Icon component for "${icon}" not found.`);
